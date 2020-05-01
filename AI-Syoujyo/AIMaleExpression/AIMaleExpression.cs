92c81b5907650c683db013c648e561bc1782c4e0
宵夜 <34893504+xiaoye97@users.noreply.github.com>
2020-05-01 22:05:53 +0800

    Update AIMaleExpression.cs
    增加全局设定功能，大幅减少工作量

--- AI-Syoujyo/AIMaleExpression/AIMaleExpression.cs
+++ AI-Syoujyo/AIMaleExpression/AIMaleExpression.cs
@@ -10,7 +10,7 @@ using System.Runtime.InteropServices;
 
 namespace xiaoye97
 {
-    [BepInPlugin("me.xiaoye97.plugin.AIMaleExpression", "AI少女男性H表情辅助插件", "1.3")]
+    [BepInPlugin("me.xiaoye97.plugin.AIMaleExpression", "AI少女男性H表情辅助插件", "1.4")]
     public class AIMaleExpression : BaseUnityPlugin
     {
         private HScene hscene;
@@ -21,6 +21,7 @@ namespace xiaoye97
         void Start()
         {
             new Harmony("me.xiaoye97.plugin.AIMaleExpression").PatchAll();
+            LoadOverrideData();
             windowId = UnityEngine.Random.Range(1000000000, 2000000000);
         }
 
@@ -59,6 +60,8 @@ namespace xiaoye97
         private HMotionEyeNeckMale.EyeNeck enTmp;
         public bool showOpenEye = true, showOpenMouth = true, showEyebrow = true, showEye = true, showMouth = true;
         public bool showNeckbehaviour, showEyebehaviour, showTargetNeck, showNeckRotA, showNeckRotB, showHeadRotA, showHeadRotB, showTargetEye, showEyeRotA, showEyeRotB;
+        public static GlobalOverrideData nowOverrideData;
+
         void ListGUI()
         {
             if (!string.IsNullOrEmpty(nowABPath)) GUILayout.Label($"路径:{nowABPath}");
@@ -197,6 +200,22 @@ namespace xiaoye97
         }
 
         #region 读写数据
+
+        public static void LoadOverrideData()
+        {
+            if (File.Exists($"{Paths.PluginPath}/AIMaleExpression/GlobalOverride.csv"))
+            {
+                //Debug.Log("男性表情插件:读取全局复写配置");
+                nowOverrideData = new GlobalOverrideData(File.ReadAllLines($"{Paths.PluginPath}/AIMaleExpression/GlobalOverride.csv"));
+            }
+            else
+            {
+                Debug.Log("男性表情插件:没有找到全局复写配置，创建新配置文件");
+                if (!Directory.Exists($"{Paths.PluginPath}/AIMaleExpression/")) Directory.CreateDirectory($"{Paths.PluginPath}/AIMaleExpression/");
+                File.WriteAllText($"{Paths.PluginPath}/AIMaleExpression/GlobalOverride.csv", "动画,list名列表(以|分割),目開き,口開き,眉,目形,口舌形,首挙動,目挙動,首タゲ,首角度,,,,,,頭角度,,,,,,目タゲ,目角度,,,,,\n");
+            }
+        }
+
         /// <summary>
         /// 保存当前数据到文件，文件路径Bepinex/list名+动画名+时间.csv
         /// </summary>
@@ -334,32 +353,8 @@ namespace xiaoye97
 
         public static HMotionEyeNeckMale.EyeNeck ParseEyeNeckFromValues(string[] vs)
         {
-            Vector3[] v;
-            HMotionEyeNeckMale.EyeNeck neck = new HMotionEyeNeckMale.EyeNeck();
-            neck.Init();
-            neck.anim = vs[0];
-            neck.openEye = int.Parse(vs[1]);
-            neck.openMouth = int.Parse(vs[2]);
-            neck.eyebrow = int.Parse(vs[3]);
-            neck.eye = int.Parse(vs[4]);
-            neck.mouth = int.Parse(vs[5]);
-            neck.Neckbehaviour = int.Parse(vs[6]);
-            neck.Eyebehaviour = int.Parse(vs[7]);
-            neck.targetNeck = int.Parse(vs[8]);
-            v = new Vector3[] { Vector3.zero, Vector3.zero };
-            v[0] = ParseV3(vs[9], vs[10], vs[11]);
-            v[1] = ParseV3(vs[12], vs[13], vs[14]);
-            neck.NeckRot = v;
-            v = new Vector3[] { Vector3.zero, Vector3.zero };
-            v[0] = ParseV3(vs[15], vs[16], vs[17]);
-            v[1] = ParseV3(vs[18], vs[19], vs[20]);
-            neck.HeadRot = v;
-            neck.targetEye = int.Parse(vs[21]);
-            v = new Vector3[] { Vector3.zero, Vector3.zero };
-            v[0] = ParseV3(vs[22], vs[23], vs[24]);
-            v[1] = ParseV3(vs[25], vs[26], vs[27]);
-            neck.EyeRot = v;
-            return neck;
+            MaleNeckData neck = new MaleNeckData(vs);
+            return neck.ToEyeNeck();
         }
 
         public static Vector3 ParseV3(string x, string y, string z)
@@ -416,7 +411,10 @@ namespace xiaoye97
                 nowList = _file;
                 if (Directory.Exists($"{Paths.PluginPath}/AIMaleExpression/"))
                 {
-                    //尝试加载CSV
+                    string[] vs = null;
+                    //先加载全局重写CSV
+                    LoadOverrideData();
+                    //尝试加载体位CSV
                     if (File.Exists($"{Paths.PluginPath}/AIMaleExpression/{_file}.csv"))
                     {
                         var lines = File.ReadAllLines($"{Paths.PluginPath}/AIMaleExpression/{_file}.csv");
@@ -426,7 +424,7 @@ namespace xiaoye97
                             {
                                 if (!string.IsNullOrEmpty(lines[i]))
                                 {
-                                    string[] vs = lines[i].Split(',');
+                                    vs = lines[i].Split(',');
                                     var lst = Traverse.Create(__instance).Field("lstEyeNeck").GetValue<List<HMotionEyeNeckMale.EyeNeck>>();
                                     lst.Add(ParseEyeNeckFromValues(vs));
                                 }
@@ -448,7 +446,7 @@ namespace xiaoye97
                                     string line = lines[i].Replace("><", ",");
                                     line = line.Replace("<", "");
                                     line = line.Replace(">", "");
-                                    string[] vs = line.Split(',');
+                                    vs = line.Split(',');
                                     var lst = Traverse.Create(__instance).Field("lstEyeNeck").GetValue<List<HMotionEyeNeckMale.EyeNeck>>();
                                     lst.Add(ParseEyeNeckFromValues(vs));
                                 }
@@ -462,12 +460,287 @@ namespace xiaoye97
             }
         }
 
+        [HarmonyPatch(typeof(HMotionEyeNeckMale), "Load")]
+        class OverrideDataPatch
+        {
+            public static void Postfix(HMotionEyeNeckMale __instance)
+            {
+                if (nowOverrideData != null)
+                {
+                    //检查全局覆盖
+                    var lst = Traverse.Create(__instance).Field("lstEyeNeck").GetValue<List<HMotionEyeNeckMale.EyeNeck>>();
+                    if (lst != null)
+                    {
+                        for (int i = 0; i < lst.Count; i++)
+                        {
+                            MaleNeckData n = new MaleNeckData(lst[i]);
+                            lst[i] = nowOverrideData.OverrideMaleNeckData(n).ToEyeNeck();
+                        }
+                    }
+                }
+            }
+        }
+
         [HarmonyPatch(typeof(ChaControl), "setPlay")]
         class HAnimPatch
         {
             public static void Postfix(string _strAnmName)
             {
                 nowAnim = _strAnmName;
+            }
+        }
+
+        public class GlobalOverrideData
+        {
+            List<GlobalOverrideRowData> DataList = new List<GlobalOverrideRowData>();
+
+            public GlobalOverrideData(string[] lines)
+            {
+                for (int i = 1; i < lines.Length; i++)
+                {
+                    if (!string.IsNullOrEmpty(lines[i]))
+                    {
+                        DataList.Add(new GlobalOverrideRowData(lines[i]));
+                    }
+                }
+            }
+
+            public MaleNeckData OverrideMaleNeckData(MaleNeckData neck)
+            {
+                //Debug.Log($"男性表情插件:动画{neck.anim}开始检查复写");
+                foreach (var d in DataList)
+                {
+                    //Debug.Log($"男性表情插件:{d.anim}");
+                    if (d.anim == neck.anim)
+                    {
+                        //Debug.Log($"男性表情插件:动画{neck.anim}在全局复写中存在，尝试进行复写");
+                        return d.OverrideMaleNeckData(neck);
+                    }
+                }
+                return neck;
+            }
+        }
+
+        public class GlobalOverrideRowData
+        {
+            public string anim;
+            public List<string> lists = new List<string>();
+            public int openEye;
+            public bool bOpenEye;
+            public int openMouth;
+            public bool bOpenMouth;
+            public int eyebrow;
+            public bool bEyeBrow;
+            public int eye;
+            public bool bEye;
+            public int mouth;
+            public bool bMouth;
+            public int Neckbehaviour;
+            public bool bNeckbehaviour;
+            public int Eyebehaviour;
+            public bool bEyebehaviour;
+            public int targetNeck;
+            public bool bTargetNeck;
+            public Vector3[] NeckRot;
+            public bool bNeckRot;
+            public Vector3[] HeadRot;
+            public bool bHeadRot;
+            public int targetEye;
+            public bool bTargetEye;
+            public Vector3[] EyeRot;
+            public bool bEyeRot;
+
+            public GlobalOverrideRowData(string line)
+            {
+                //Debug.Log("男性表情插件:开始解析 " + line);
+                string[] vs = line.Split(',');
+                Vector3[] v;
+                anim = vs[0];
+                //Debug.Log($"男性表情插件:读取到动画{anim}的配置");
+                string[] ls = vs[1].Split('|');
+                foreach (string l in ls)
+                {
+                    if (!string.IsNullOrEmpty(l)) lists.Add(l);
+                }
+                if (!string.IsNullOrEmpty(vs[2]))
+                {
+                    openEye = int.Parse(vs[2]);
+                    bOpenEye = true;
+                }
+                if (!string.IsNullOrEmpty(vs[3]))
+                {
+                    openMouth = int.Parse(vs[3]);
+                    bOpenMouth = true;
+                }
+                if (!string.IsNullOrEmpty(vs[4]))
+                {
+                    eyebrow = int.Parse(vs[4]);
+                    bEyeBrow = true;
+                }
+                if (!string.IsNullOrEmpty(vs[5]))
+                {
+                    eye = int.Parse(vs[5]);
+                    bEye = true;
+                }
+                if (!string.IsNullOrEmpty(vs[6]))
+                {
+                    mouth = int.Parse(vs[6]);
+                    bMouth = true;
+                }
+                if (!string.IsNullOrEmpty(vs[7]))
+                {
+                    Neckbehaviour = int.Parse(vs[7]);
+                    bNeckbehaviour = true;
+                }
+                if (!string.IsNullOrEmpty(vs[8]))
+                {
+                    Eyebehaviour = int.Parse(vs[8]);
+                    bEyebehaviour = true;
+                }
+                if (!string.IsNullOrEmpty(vs[9]))
+                {
+                    targetNeck = int.Parse(vs[9]);
+                    bTargetNeck = true;
+                }
+                if (!string.IsNullOrEmpty(vs[10]))
+                {
+                    v = new Vector3[] { Vector3.zero, Vector3.zero };
+                    v[0] = ParseV3(vs[10], vs[11], vs[12]);
+                    v[1] = ParseV3(vs[13], vs[14], vs[15]);
+                    NeckRot = v;
+                    bNeckRot = true;
+                }
+                if (!string.IsNullOrEmpty(vs[16]))
+                {
+                    v = new Vector3[] { Vector3.zero, Vector3.zero };
+                    v[0] = ParseV3(vs[16], vs[17], vs[18]);
+                    v[1] = ParseV3(vs[19], vs[20], vs[21]);
+                    HeadRot = v;
+                    bHeadRot = true;
+                }
+                if (!string.IsNullOrEmpty(vs[22]))
+                {
+                    targetEye = int.Parse(vs[22]);
+                    bTargetEye = true;
+                }
+                if (!string.IsNullOrEmpty(vs[23]))
+                {
+                    v = new Vector3[] { Vector3.zero, Vector3.zero };
+                    v[0] = ParseV3(vs[23], vs[24], vs[25]);
+                    v[1] = ParseV3(vs[26], vs[27], vs[28]);
+                    EyeRot = v;
+                    bEyeRot = true;
+                }
+            }
+
+            public MaleNeckData OverrideMaleNeckData(MaleNeckData neck)
+            {
+                //Debug.Log("男性表情插件:GlobalOverrideRowData:lists.Count:" + lists.Count);
+                //foreach (var l in lists)
+                //{
+                //    Debug.Log("男性表情插件:GlobalOverrideRowData:lists:" + l);
+                //}
+                //Debug.Log("男性表情插件:nowList:" + nowList);
+                if (lists.Count == 0 || lists.Contains(nowList))
+                {
+                    //Debug.Log("男性表情插件:该姿势符合复写条件，开始复写");
+                    if (bOpenEye) neck.openEye = openEye;
+                    if (bOpenMouth) neck.openMouth = openMouth;
+                    if (bEyeBrow) neck.eyebrow = eyebrow;
+                    if (bEye) neck.eye = eye;
+                    if (bMouth) neck.mouth = mouth;
+                    if (bNeckbehaviour) neck.Neckbehaviour = Neckbehaviour;
+                    if (bEyebehaviour) neck.Eyebehaviour = Eyebehaviour;
+                    if (bTargetNeck) neck.targetNeck = targetNeck;
+                    if (bNeckRot) neck.NeckRot = NeckRot;
+                    if (bHeadRot) neck.HeadRot = HeadRot;
+                    if (bTargetEye) neck.targetEye = targetEye;
+                    if (bEyeRot) neck.EyeRot = EyeRot;
+                }
+                return neck;
+            }
+        }
+
+        public class MaleNeckData
+        {
+            public string anim;
+            public int openEye;
+            public int openMouth;
+            public int eyebrow;
+            public int eye;
+            public int mouth;
+            public int Neckbehaviour;
+            public int Eyebehaviour;
+            public int targetNeck;
+            public Vector3[] NeckRot;
+            public Vector3[] HeadRot;
+            public int targetEye;
+            public Vector3[] EyeRot;
+
+            //从原版中拷贝数据构造
+            public MaleNeckData(HMotionEyeNeckMale.EyeNeck neck)
+            {
+                anim = neck.anim;
+                openEye = neck.openEye;
+                openMouth = neck.openMouth;
+                eyebrow = neck.eyebrow;
+                eye = neck.eye;
+                mouth = neck.mouth;
+                Neckbehaviour = neck.Neckbehaviour;
+                Eyebehaviour = neck.Eyebehaviour;
+                targetNeck = neck.targetNeck;
+                NeckRot = neck.NeckRot;
+                HeadRot = neck.HeadRot;
+                targetEye = neck.targetEye;
+                EyeRot = neck.EyeRot;
+            }
+
+            //从list的csv行数据中构造
+            public MaleNeckData(string[] vs)
+            {
+                Vector3[] v;
+                anim = vs[0];
+                openEye = int.Parse(vs[1]);
+                openMouth = int.Parse(vs[2]);
+                eyebrow = int.Parse(vs[3]);
+                eye = int.Parse(vs[4]);
+                mouth = int.Parse(vs[5]);
+                Neckbehaviour = int.Parse(vs[6]);
+                Eyebehaviour = int.Parse(vs[7]);
+                targetNeck = int.Parse(vs[8]);
+                v = new Vector3[] { Vector3.zero, Vector3.zero };
+                v[0] = ParseV3(vs[9], vs[10], vs[11]);
+                v[1] = ParseV3(vs[12], vs[13], vs[14]);
+                NeckRot = v;
+                v = new Vector3[] { Vector3.zero, Vector3.zero };
+                v[0] = ParseV3(vs[15], vs[16], vs[17]);
+                v[1] = ParseV3(vs[18], vs[19], vs[20]);
+                HeadRot = v;
+                targetEye = int.Parse(vs[21]);
+                v = new Vector3[] { Vector3.zero, Vector3.zero };
+                v[0] = ParseV3(vs[22], vs[23], vs[24]);
+                v[1] = ParseV3(vs[25], vs[26], vs[27]);
+                EyeRot = v;
+            }
+
+            //转换为原版表情
+            public HMotionEyeNeckMale.EyeNeck ToEyeNeck()
+            {
+                HMotionEyeNeckMale.EyeNeck neck = new HMotionEyeNeckMale.EyeNeck();
+                neck.anim = anim;
+                neck.openEye = openEye;
+                neck.openMouth = openMouth;
+                neck.eyebrow = eyebrow;
+                neck.eye = eye;
+                neck.mouth = mouth;
+                neck.Neckbehaviour = Neckbehaviour;
+                neck.Eyebehaviour = Eyebehaviour;
+                neck.targetNeck = targetNeck;
+                neck.NeckRot = NeckRot;
+                neck.HeadRot = HeadRot;
+                neck.targetEye = targetEye;
+                neck.EyeRot = EyeRot;
+                return neck;
             }
         }
     }
