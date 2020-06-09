3b129b780f269598e736eb1459b9810ed4d90abd
宵夜 <34893504+xiaoye97@users.noreply.github.com>
2020-06-10 00:49:57 +0800

    添加HS2 SideLoader拓展插件
    添加HS2 SideLoader拓展插件
    此插件可以让SideLoader除了.zip和.zipmod之外也支持.mod后缀名

---
+++ HS2/HS2_Sideloader_Addion/HS2_Sideloader_Addion/Sideloader_Addion.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using BepInEx;
+using HarmonyLib;
+
+namespace me.xiaoye97.plugin.HS2.SideLoader_Addion
+{
+    [BepInPlugin("_me.xiaoye97.plugin.HS2.SideLoader_Addion", "SideLoader扩展", "1.0")]
+    public class SideLoader_Addion : BaseUnityPlugin
+    {
+        public static Harmony harmony;
+        public static bool tmpFlag, loading;
+
+        void Awake()
+        {
+            harmony = new Harmony("me.xiaoye97.plugin.HS2.SideLoader_Addion");
+            harmony.Patch(
+                AccessTools.Method(typeof(Sideloader.Sideloader), "LoadModsFromDirectories"),
+                new HarmonyMethod(AccessTools.Method(typeof(SideLoader_Addion), "LoadDirPatchPre")),
+                new HarmonyMethod(AccessTools.Method(typeof(SideLoader_Addion), "LoadDirPatchPost")));
+        }
+
+        public static bool LoadDirPatchPre()
+        {
+            loading = true;
+            harmony.Patch(
+                AccessTools.Method(typeof(String), "EndsWith", new Type[] { typeof(string), typeof(StringComparison) }),
+                new HarmonyMethod(AccessTools.Method(typeof(SideLoader_Addion), "StringEndWithPre")), null);
+            return true;
+        }
+
+        public static void LoadDirPatchPost()
+        {
+            loading = false;
+        }
+
+        public static bool StringEndWithPre(string value, String __instance, ref bool __result)
+        {
+            if (!loading) return true;
+            if (value == ".zip" || value == ".zipmod")
+            {
+                if (!tmpFlag)
+                {
+                    tmpFlag = true;
+                    if (__instance.EndsWith(".mod", StringComparison.OrdinalIgnoreCase))
+                    {
+                        __result = true;
+                        tmpFlag = false;
+                        return false;
+                    }
+                    tmpFlag = false;
+                }
+            }
+            return true;
+        }
+    }
+}
