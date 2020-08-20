4e607fa12b152947fa2c8abe0692c666f1c3bfe4
宵夜 <34893504+xiaoye97@users.noreply.github.com>
2020-08-21 00:29:24 +0800

    Update RuntimeUnityEditorAddion.cs
    更新显示颜色功能

--- Unity/RuntimeUnityEditorAddion/RuntimeUnityEditorAddion.cs
+++ Unity/RuntimeUnityEditorAddion/RuntimeUnityEditorAddion.cs
@@ -6,22 +6,25 @@ using System.Linq;
 using BepInEx.Configuration;
 using System.Collections.Generic;
 using RuntimeUnityEditor.Core.Utils;
+using RuntimeUnityEditor.Core.Inspector;
 using RuntimeUnityEditor.Core.ObjectTree;
+using RuntimeUnityEditor.Core.Inspector.Entries;
 
 namespace RuntimeUnityEditorAddion
 {
-    [BepInPlugin("me.xiaoye97.plugin.Unity.RuntimeUnityEditorAddion", "运行时Unity编辑器扩展", "1.0")]
+    [BepInPlugin("me.xiaoye97.plugin.Unity.RuntimeUnityEditorAddion", "运行时Unity编辑器扩展", "1.1")]
     public class RuntimeUnityEditorAddion : BaseUnityPlugin
     {
-        public static ConfigEntry<bool> filterUnload;
-        public static ConfigEntry<bool> disableSort;
+        public static ConfigEntry<bool> filterUnload, disableSort, showColor;
         void Start()
         {
             filterUnload = Config.Bind<bool>("Setting", "Filter Unload", true);
             disableSort = Config.Bind<bool>("Setting", "Diasble Sort", true);
+            showColor = Config.Bind<bool>("Setting", "Show Color", true);
             new Harmony("me.xiaoye97.plugin.Unity.RuntimeUnityEditorAddion").PatchAll();
         }
 
+        #region 过滤未加载的物体
         [HarmonyPatch(typeof(GameObjectSearcher), "FindAllRootGameObjects")]
         class FindPatch
         {
@@ -39,7 +42,9 @@ namespace RuntimeUnityEditorAddion
                 __result = objs;
             }
         }
+        #endregion
 
+        #region 禁止按字母排序
         [HarmonyPatch(typeof(GameObjectSearcher), "Refresh")]
         class RefreshPatch
         {
@@ -77,5 +82,43 @@ namespace RuntimeUnityEditorAddion
                 return false;
             }
         }
+        #endregion
+
+        #region 显示颜色
+        public static Color cacheColor, backColor;
+        private static bool startShowColor = false;
+        [HarmonyPatch(typeof(Inspector), "DrawSingleContentEntry")]
+        class ColorPatch
+        {
+            public static bool Prefix(ICacheEntry entry)
+            {
+                if (entry.TypeName() != "UnityEngine.Color") return true;
+                cacheColor = (Color)entry.GetValue();
+                backColor = GUI.contentColor;
+                startShowColor = true;
+                return true;
+            }
+        }
+
+        [HarmonyPatch(typeof(GUILayout), "Label", new Type[] {typeof(string), typeof(GUILayoutOption[]) })]
+        class ColorPatch2
+        {
+            public static bool Prefix(ref GUILayoutOption[] options)
+            {
+                if (!showColor.Value) return true;
+                if (!startShowColor) return true;
+                startShowColor = false;
+                GUI.contentColor = cacheColor;
+                GUILayout.Label("██", GUILayout.Width(18));
+                options = new GUILayoutOption[]
+                {
+                    GUILayout.Width(148f),
+                    GUILayout.MaxWidth(148f)
+                };
+                GUI.contentColor = backColor;
+                return true;
+            }
+        }
+        #endregion
     }
 }
