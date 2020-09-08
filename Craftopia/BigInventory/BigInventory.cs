109d8605d4722d06cca5671db4910f1df762bac0
宵夜 <34893504+xiaoye97@users.noreply.github.com>
2020-09-08 14:48:02 +0800

    Create BigInventory.cs
    添加创世理想乡的插件 大背包
    将背包数量改为8x8

---
+++ Craftopia/BigInventory/BigInventory.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using BepInEx;
+using Oc.Item;
+using Oc.Item.UI;
+using System.Collections.Generic;
+
+namespace BigInventory
+{
+    [BepInPlugin("me.xiaoye97.plugin.Craftopia.BigInventory", "大背包", "1.0")]
+    public class BigInventory : BaseUnityPlugin
+    {
+        public static bool finished = false;
+
+        void Update()
+        {
+            if (finished) return;
+			if (SingletonMonoBehaviour<OcItemUI_InventoryMng>.Inst != null)
+			{
+				finished = true;
+				OcItemUI_InventoryMng inst = SingletonMonoBehaviour<OcItemUI_InventoryMng>.Inst;
+				List<OcItemUI_Cell_List> list = new List<OcItemUI_Cell_List>();
+				foreach (OcItemStack stack in inst.GetStacksNotNull(true))
+				{
+					list.Add(inst.GetBelongList(stack));
+				}
+				for (int i = 0; i < list.Count; i++)
+				{
+					if (list[i] != null && list[i].Size != 64)
+					{
+						list[i].SetSize(64);
+					}
+				}
+			}
+		}
+    }
+}
