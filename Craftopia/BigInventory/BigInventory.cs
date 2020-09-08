5d17114c513d999998c12e9c70e851359c4de4a8
宵夜 <34893504+xiaoye97@users.noreply.github.com>
2020-09-08 18:45:19 +0800

    Update BigInventory.cs

--- Craftopia/BigInventory/BigInventory.cs
+++ Craftopia/BigInventory/BigInventory.cs
@@ -8,33 +8,33 @@ namespace BigInventory
     [BepInPlugin("me.xiaoye97.plugin.Craftopia.BigInventory", "大背包", "1.1")]
     public class BigInventory : BaseUnityPlugin
     {
-		public static int InventorySize = 64;
+        public static int InventorySize = 64;
 
-		void Start()
+        void Start()
         {
-			new Harmony("me.xiaoye97.plugin.Craftopia.BigInventory").PatchAll();
+            new Harmony("me.xiaoye97.plugin.Craftopia.BigInventory").PatchAll();
         }
 
-		[HarmonyPatch(typeof(OcGameMng), "OnGameSceneSetUpFinish")]
-		class InventoryPatch
+        [HarmonyPatch(typeof(OcGameMng), "OnGameSceneSetUpFinish")]
+        class InventoryPatch
         {
-			public static void Postfix()
+            public static void Postfix()
             {
-				FixInventory();
-			}
+                FixInventory();
+            }
         }
 
-		public static void FixInventory()
-		{
-			var inst = SingletonMonoBehaviour<OcItemUI_InventoryMng>.Inst;
-			if (inst != null)
-			{
-				Traverse.Create(inst).Field("equipmentList").Method("SetSize", 64).GetValue();
-				Traverse.Create(inst).Field("consumptionList").Method("SetSize", 64).GetValue();
-				Traverse.Create(inst).Field("materialList").Method("SetSize", 64).GetValue();
-				Traverse.Create(inst).Field("relicList").Method("SetSize", 64).GetValue();
-				Traverse.Create(inst).Field("buildingList").Method("SetSize", 64).GetValue();
-			}
-		}
-	}
+        public static void FixInventory()
+        {
+            var inst = SingletonMonoBehaviour<OcItemUI_InventoryMng>.Inst;
+            if (inst != null)
+            {
+                Traverse.Create(inst).Field("equipmentList").Method("SetSize", 64).GetValue();
+                Traverse.Create(inst).Field("consumptionList").Method("SetSize", 64).GetValue();
+                Traverse.Create(inst).Field("materialList").Method("SetSize", 64).GetValue();
+                Traverse.Create(inst).Field("relicList").Method("SetSize", 64).GetValue();
+                Traverse.Create(inst).Field("buildingList").Method("SetSize", 64).GetValue();
+            }
+        }
+    }
 }
