0c589e383bf68eeeec2c1b8a87b81eac92343fd7
宵夜 <34893504+xiaoye97@users.noreply.github.com>
2021-01-04 12:15:44 +0800

    Create GameSpeed.cs
    添加插件：
    《觅长生》游戏变速插件

---
+++ MiChangSheng/GameSpeed/GameSpeed.cs
@@ -0,0 +1,44 @@
+﻿using BepInEx;
+using UnityEngine;
+using BepInEx.Configuration;
+
+namespace GameSpeed
+{
+    [BepInPlugin("me.xiaoye97.plugin.MiChangSheng.GameSpeed", "游戏变速", "1.0")]
+    public class GameSpeed : BaseUnityPlugin
+    {
+        ConfigEntry<float> SpeedConfig;
+        ConfigEntry<KeyCode> KeyConfig;
+        bool isChange;
+        public bool IsChange
+        {
+            get { return isChange; }
+            set
+            {
+                if(value)
+                {
+                    Time.timeScale = SpeedConfig.Value;
+                }
+                else
+                {
+                    Time.timeScale = 1;
+                }
+                isChange = value;
+            }
+        }
+        void Start()
+        {
+            SpeedConfig = Config.Bind<float>("config", "Speed", 2f, "自定义变速倍数，变速范围0.2-5");
+            SpeedConfig.Value = Mathf.Clamp(SpeedConfig.Value, 0.2f, 5);
+            KeyConfig = Config.Bind<KeyCode>("config", "HotKey", KeyCode.B, "自定义热键");
+        }
+
+        void Update()
+        {
+            if(Input.GetKeyDown(KeyConfig.Value))
+            {
+                IsChange = !IsChange;
+            }
+        }
+    }
+}
