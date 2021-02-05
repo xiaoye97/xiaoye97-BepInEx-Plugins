08d0257a325a9707f3c794682446deea53e36847
宵夜 <34893504+xiaoye97@users.noreply.github.com>
2021-02-05 16:53:29 +0800

    更新插件
    更新插件《戴森球计划》
    LDBTool
    将添加数据拆分为前置添加和后置添加，以变添加翻译Proto

--- Dyson Sphere Program/LDBTool/LDBTool.cs
+++ Dyson Sphere Program/LDBTool/LDBTool.cs
@@ -8,23 +8,38 @@ using System.Collections.Generic;
 
 namespace xiaoye97
 {
-    [BepInPlugin("me.xiaoye97.plugin.Dyson.LDBTool", "LDBTool", "1.1")]
+    [BepInPlugin("me.xiaoye97.plugin.Dyson.LDBTool", "LDBTool", "1.2")]
     public class LDBTool : BaseUnityPlugin
     {
-        private static Dictionary<ProtoType, List<Proto>> ReadyToAdd = new Dictionary<ProtoType, List<Proto>>();
-        public static Action AddDataAction;
+        private static Dictionary<ProtoType, List<Proto>> PreToAdd = new Dictionary<ProtoType, List<Proto>>();
+        private static Dictionary<ProtoType, List<Proto>> PostToAdd = new Dictionary<ProtoType, List<Proto>>();
+        private static Dictionary<ProtoType, List<Proto>> TotalDict = new Dictionary<ProtoType, List<Proto>>();
+        public static Action PreAddDataAction, PostAddDataAction;
         public static Action<Proto> EditDataAction;
         public static ConfigEntry<bool> ShowProto;
         public static ConfigEntry<KeyCode> ShowProtoHotKey;
 
         /// <summary>
-        /// 添加数据
+        /// 在VFPreload.PreloadThread之前添加数据
         /// </summary>
-        public static void AddProto(ProtoType protoType, Proto proto)
+        public static void PreAddProto(ProtoType protoType, Proto proto)
         {
-            if (!ReadyToAdd[protoType].Contains(proto))
+            if (!PreToAdd[protoType].Contains(proto))
             {
-                ReadyToAdd[protoType].Add(proto);
+                PreToAdd[protoType].Add(proto);
+                TotalDict[protoType].Add(proto);
+            }
+        }
+
+        /// <summary>
+        /// 在VFPreload.PreloadThread之后添加数据
+        /// </summary>
+        public static void PostAddProto(ProtoType protoType, Proto proto)
+        {
+            if (!PostToAdd[protoType].Contains(proto))
+            {
+                PostToAdd[protoType].Add(proto);
+                TotalDict[protoType].Add(proto);
             }
         }
 
@@ -32,7 +47,9 @@ namespace xiaoye97
         {
             for (int i = 0; i <= (int)ProtoType.Vein; i++)
             {
-                ReadyToAdd.Add((ProtoType)i, new List<Proto>());
+                PreToAdd.Add((ProtoType)i, new List<Proto>());
+                PostToAdd.Add((ProtoType)i, new List<Proto>());
+                TotalDict.Add((ProtoType)i, new List<Proto>());
             }
         }
 
@@ -62,16 +79,9 @@ namespace xiaoye97
             }
         }
 
-        [HarmonyPostfix, HarmonyPatch(typeof(VFPreload), "InvokeOnLoadWorkEnded")]
-        public static void VFPreloadPatch()
+        private static void AddProtos(Dictionary<ProtoType, List<Proto>> datas)
         {
-            Debug.Log("[LDBTool]Loading...");
-            if (AddDataAction != null)
-            {
-                AddDataAction();
-                AddDataAction = null;
-            }
-            foreach (var kv in ReadyToAdd)
+            foreach (var kv in datas)
             {
                 if (kv.Value.Count > 0)
                 {
@@ -90,6 +100,30 @@ namespace xiaoye97
                     else if (kv.Key == ProtoType.Vein) AddProtosToSet(LDB.veins, kv.Value);
                 }
             }
+        }
+
+        [HarmonyPrefix, HarmonyPatch(typeof(VFPreload), "InvokeOnLoadWorkEnded")]
+        public static void VFPreloadPrePatch()
+        {
+            Debug.Log("[LDBTool]Pre Loading...");
+            if (PreAddDataAction != null)
+            {
+                PreAddDataAction();
+                PreAddDataAction = null;
+            }
+            AddProtos(PreToAdd);
+        }
+
+        [HarmonyPostfix, HarmonyPatch(typeof(VFPreload), "InvokeOnLoadWorkEnded")]
+        public static void VFPreloadPostPatch()
+        {
+            Debug.Log("[LDBTool]Post Loading...");
+            if (PostAddDataAction != null)
+            {
+                PostAddDataAction();
+                PostAddDataAction = null;
+            }
+            AddProtos(PostToAdd);
             List<Proto> allProto = new List<Proto>();
             foreach (var p in LDB.advisorTips.dataArray) allProto.Add(p);
             foreach (var p in LDB.audios.dataArray) allProto.Add(p);
@@ -132,7 +166,7 @@ namespace xiaoye97
         [HarmonyPostfix, HarmonyPatch(typeof(GameHistoryData), "Import")]
         public static void HistoryPatch(GameHistoryData __instance)
         {
-            foreach (var proto in ReadyToAdd[ProtoType.Recipe])
+            foreach (var proto in TotalDict[ProtoType.Recipe])
             {
                 var recipe = proto as RecipeProto;
                 if (recipe.preTech != null)
@@ -171,16 +205,24 @@ namespace xiaoye97
                         ArrayAddItem<RecipeProto>(ref proto.preTech.unlockRecipeArray, proto);
                     }
                 }
-                Debug.Log($"[LDBTool]Add {protos[i].ID} {protos[i].Name} to {protoSet.GetType().Name}.");
+                Debug.Log($"[LDBTool]Add {protos[i].ID} {protos[i].Name.Translate()} to {protoSet.GetType().Name}.");
             }
             var dataIndices = new Dictionary<int, int>();
             for (int i = 0; i < protoSet.dataArray.Length; i++)
             {
-                protoSet.dataArray[i].name = protoSet.dataArray[i].Name;
                 protoSet.dataArray[i].sid = protoSet.dataArray[i].SID;
                 dataIndices[protoSet.dataArray[i].ID] = i;
             }
             Traverse.Create(protoSet).Field("dataIndices").SetValue(dataIndices);
+            if (protoSet is ProtoSet<StringProto>)
+            {
+                var nameIndices = new Dictionary<string, int>();
+                for (int i = 0; i < protoSet.dataArray.Length; i++)
+                {
+                    nameIndices[protoSet.dataArray[i].Name] = i;
+                }
+                Traverse.Create(protoSet).Field("nameIndices").SetValue(nameIndices);
+            }
         }
 
         /// <summary>
