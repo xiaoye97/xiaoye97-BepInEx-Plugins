5d102417fe5722d366ae943906ef890735f62b4f
xiaoye97 <1066666683@qq.com>
2021-06-20 13:15:21 +0800

    更新Craftopia大背包mod
    修复了多余背包里的物品会掉出的问题

--- Craftopia/BigInventory/BigInventory.cs
+++ Craftopia/BigInventory/BigInventory.cs
@@ -18,8 +18,19 @@ namespace BigInventory
         [HarmonyPostfix, HarmonyPatch(typeof(OcItemUI_Cell_List_Inventory), "ConvertFromSaveData")]
         public static void GamePatch(OcItemUI_Cell_List_Inventory __instance)
         {
-            Debug.Log($"设置背包{__instance.name}的大小到{InventorySize}");
-            __instance.SetSize(InventorySize);
+            OcItemUI_InventoryMng instUISS2 = UISceneSingleton<OcItemUI_InventoryMng>.InstUISS;
+            if ((int)__instance.itemType < instUISS2.advancedExtendInventory.Length)
+            {
+                if (__instance.Size < InventorySize)
+                {
+                    Debug.Log($"将背包{__instance.name}({__instance.itemType})的大小从到{__instance.Size}增加到{InventorySize}");
+                    instUISS2.IncreaseListSize(__instance.itemType, InventorySize - __instance.Size, true);
+                }
+                else
+                {
+                    Debug.Log($"背包{__instance.name}({__instance.itemType})的大小不需要变化");
+                }
+            }
         }
     }
 }
