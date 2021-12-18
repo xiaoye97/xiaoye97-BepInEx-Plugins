9f83e116101a066d7f1664bc69adf726768c10e6
xiaoye97 <1163537847@qq.com>
2021-12-18 12:26:28 +0800

    Update BigInventory.cs

--- Craftopia/BigInventory/BigInventory.cs
+++ Craftopia/BigInventory/BigInventory.cs
@@ -1,3 +1,4 @@
+using Oc;
 using System;
 using BepInEx;
 using HarmonyLib;
@@ -19,32 +20,32 @@ namespace BigInventory
             Logger.LogInfo("BigInventory Start");
         }
 
-        [HarmonyPostfix, HarmonyPatch(typeof(OcItemUI_Cell_List_Inventory), "ConvertFromSaveData")]
-        public static void GamePatch(OcItemUI_Cell_List_Inventory __instance)
-        {
-            OcItemUI_InventoryMng instUISS2 = UISceneSingleton<OcItemUI_InventoryMng>.InstUISS;
-            if ((int)__instance.itemType < instUISS2.advancedExtendInventory.Length)
-            {
-                if (__instance.Size < InventorySize)
-                {
-                    logger.LogInfo($"将背包{__instance.name}({__instance.itemType})的大小从到{__instance.Size}增加到{InventorySize}");
-                    instUISS2.IncreaseListSize(__instance.itemType, InventorySize - __instance.Size, false);
-                }
-                else
-                {
-                    logger.LogInfo($"背包{__instance.name}({__instance.itemType})的大小不需要变化");
-                }
-            }
-        }
-
         [HarmonyPrefix, HarmonyPatch(typeof(OcItemUI_Cell_List), "InitOnAwake", new Type[] { typeof(int) })]
-        public static bool GamePatch2(OcItemUI_Cell_List __instance, ref int size)
+        public static bool GamePatch(OcItemUI_Cell_List __instance, ref int size)
         {
             OcItemUI_InventoryMng instUISS2 = UISceneSingleton<OcItemUI_InventoryMng>.InstUISS;
             if ((int)__instance.itemType < instUISS2.advancedExtendInventory.Length)
             {
                 logger.LogInfo($"将{__instance.name} {__instance.itemType}的初始化尺寸从{size}设为{InventorySize}");
                 size = InventorySize;
+            }
+            return true;
+        }
+
+        [HarmonyPrefix, HarmonyPatch(typeof(OcItemUI_Cell_List), "InitOnAwake", new Type[] { })]
+        public static bool GamePatch2(OcItemUI_Cell_List __instance)
+        {
+            var _this = __instance;
+            if (!_this.list.IsNullOrEmpty<OcItemUI_Cell>())
+            {
+                if (_this.list.Count < _this.Size)
+                {
+                    logger.LogInfo($"{__instance.name} {__instance.itemType}的list.Count {_this.list.Count} < Size {_this.Size}，进行补充");
+                    for (int i = _this.list.Count; i < _this.Size; i++)
+                    {
+                        _this.list.Add(_this.InstCell(i));
+                    }
+                }
             }
             return true;
         }
